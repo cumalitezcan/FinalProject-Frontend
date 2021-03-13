5a658fb2484acb79b76160f43c8904c9b2d801a7
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-03-14 00:02:10 +0300

    addedFiltering

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RESOURCE_CACHE_PROVIDER } from '@angular/platform-browser-dynamic';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productsResponseModel';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -15,12 +15,18 @@ export class ProductComponent implements OnInit {
   dataLoaded = false;
 
 
-  
-
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }else {
+        this.getProducts()
+      }
+    })
+
   }
 
   getProducts(){
@@ -28,7 +34,14 @@ export class ProductComponent implements OnInit {
         this.products = response.data,
         this.dataLoaded=true;
       })
-      
   }
+
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products = response.data,
+      this.dataLoaded=true;
+    })
+}
+
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,22 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ProductResponseModel } from '../models/productsResponseModel';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  apiUrl = "https://localhost:44303/api/products/getall";
+  apiUrl = "https://localhost:44303/api/";
+
 
   constructor(private httpClient:HttpClient) { }
 
-  getProducts():Observable<ProductResponseModel>{
-   return this.httpClient.get<ProductResponseModel>(this.apiUrl);
-   
+  getProducts():Observable<ListResponseModel<Product>>{
+    let newPath= this.apiUrl + "products/getall";
+   return this.httpClient.get<ListResponseModel<Product>>(newPath);
     }
 
-}
+    getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
+      let newPath= this.apiUrl + "products/getbycategory?categoryId="+categoryId;
+      return this.httpClient.get<ListResponseModel<Product>>(newPath);
+       }
+  }
 
   
 
